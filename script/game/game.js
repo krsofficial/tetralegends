00466e5f04b0349ac205356b759ffcd548b849ce
Sub2Ninah <70987554+Sub2Ninah@users.noreply.github.com>
2025-09-28 22:53:08 +0800

    Update game.js

--- script/game/game.js
+++ script/game/game.js
@@ -687,8 +687,8 @@ export default class Game {
 		(game.settings.height <= 10 && game.settings.width <= 5)
 	  )
 	  {
-	  game[element].width = game[element].clientWidth / 2
-      game[element].height = game[element].clientHeight / 2
+	  game[element].width = Math.floor(game[element].clientWidth / 2)
+      game[element].height = Math.floor(game[element].clientHeight / 2)
 	  } else {
 	  game[element].width = game[element].clientWidth
       game[element].height = game[element].clientHeight
