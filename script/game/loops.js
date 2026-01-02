879c8335dff0f30f1ce87dd68004c2d489deee5a
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2026-01-03 01:30:52 +0800

    t

--- script/game/loops.js
+++ script/game/loops.js
@@ -8931,18 +8931,11 @@ export const loops = {
 	  } else {
 		  game.piece.areLimitLineModifier = 0
 	  }
-	  if (game.stat.level >= 15 && game.musicProgression < 1) {
+	  if (game.stat.level >= 25 && game.musicProgression < 1) {
 		if (game.stat.piece > 0 || game.timePassed > 0) {
           sound.killBgm()
           sound.playBgm(game.settings.music[1], game.type)
 		  game.musicProgression = 1
-        }
-      }
-	  if (game.stat.level >= 25 && game.musicProgression < 2) {
-		if (game.stat.piece > 0 || game.timePassed > 0) {
-          sound.killBgm()
-          sound.playBgm(game.settings.music[2], game.type)
-		  game.musicProgression = 2
         }
       }
       //game.stat.entrydelay = `${ARE_TABLE[calcLevel]}ms`
